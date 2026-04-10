300711d0e6b22ec14b91b8ea9716f8535f34395e
Diabltica <diabltica@gmail.com>
2026-04-10 09:32:11 +0200

    add bidirectional com to sync host and controller

--- client/client.py
+++ client/client.py
@@ -1,6 +1,6 @@
 import sys
 import serial
-from monitorcontrol import get_monitors
+from monitorcontrol import get_monitors, InputSource
 from monitorcontrol import vcp
 
 def connectSerial(port, baudrate, timeout):
@@ -12,46 +12,67 @@ def connectSerial(port, baudrate, timeout):
         print(f"Error connecting to serial port: {e}")
         return None
 
-def processCommand(monitor, command, state):
-    inputPerso=["DVI1","HDMI1","HDMI1"]
-    inputPro=["HDMI1","HDMI1","ANALOG1"]
+def processCommand(monitors, command, state):
+    global inputPerso, inputPro
     
     if state[command] == 0:
-        switchInput(monitor[command], inputPro[command])
+        setMonitorInput(monitors[command], inputPro[command])
         state[command] = 1
 
     elif state[command] == 1:
-        switchInput(monitor[command], inputPerso[command])
+        setMonitorInput(monitors[command], inputPerso[command])
         state[command] = 0
-    
     return state
 
-def switchInput(monitor, newInput):
+def setMonitorInput(monitors, newInput):
     try:
-        with monitor:
-            monitor.set_input_source(newInput)
+        with monitors:
+            monitors.set_input_source(newInput)
     except vcp.VCPError:
         print("error")
+
+def getMonitorInputSource(monitors):
+    sources = []
+    try:
+        for m in monitors:
+            with m:
+                sources.append(InputSource(m.get_input_source()).name)
+        return sources
+    except vcp.VCPError:
+        print("error")
+
+def syncState(state, monitors):
+    global inputPerso
+    actualSource = None
+    while actualSource == None:
+        actualSource = getMonitorInputSource(monitors)
+
+    for i in range(len(actualSource)):
+            state[i] = ((inputPerso[i] == actualSource[i])+1)%2
+            print("input perso: "+ inputPerso[i])
+            print("actual input: "+ actualSource[i])
+            print("state: "+ str(state[i]))
+    return state
+
+def sendStateToControler(state, ser):
+    ser.write(bytes((str(state[0])+str(state[1])+str(state[2])).encode()))
 
 if "__main__" == __name__:
     PORT = 'COM15'  # Change to your COM port
     BAUDRATE = 115200
     TIMEOUT = 1
+    inputPerso=["DVI1","HDMI1","HDMI1"]
+    inputPro=["HDMI1","HDMI1","ANALOG1"]
     state = [0,0,0] # 0 perso / 1 pro 
     ser = connectSerial(PORT, BAUDRATE, TIMEOUT)
     if ser == None:
         sys.exit(1)
-    monitor = get_monitors()
+    monitors = get_monitors()
     while(1):
+        state = syncState(state, monitors)
+        sendStateToControler(state, ser)
         line = ser.readline().decode("utf-8");
         if line != "\n" and line != "":
             print(line)
-            state = processCommand(monitor, int(line), state)
-    
-
-# to get monitor model to differentiate them
-# from monitorcontrol import get_monitors
-
-# for monitor in get_monitors():
-#     with monitor:
-#         print(monitor.get_vcp_capabilities())
+            processCommand(monitors, int(line), state)
+        
--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,4 +1,4 @@
-//#include <FastLED.h>
+#include <FastLED.h>
 
 #define LED_PIN 15
 #define LED_COUNT 3
@@ -7,17 +7,21 @@
 #define SW2_PIN 12
 #define SW3_PIN 13
 
-//CRGB leds[LED_COUNT];
+CRGB leds[LED_COUNT];
 bool isSWPressed[3] = {false,false,false};
+int state[3] =  {0,0,0};
 
-ICACHE_RAM_ATTR void interruptSW1() {
+IRAM_ATTR void interruptSW1() {
+  detachInterrupt(digitalPinToInterrupt(SW1_PIN));
   isSWPressed[0] = true;
 }
 
-ICACHE_RAM_ATTR void interruptSW2() {
+IRAM_ATTR void interruptSW2() {
+  detachInterrupt(digitalPinToInterrupt(SW2_PIN));
   isSWPressed[1] = true;
 }
-ICACHE_RAM_ATTR void interruptSW3() {
+IRAM_ATTR void interruptSW3() {
+  detachInterrupt(digitalPinToInterrupt(SW3_PIN));
   isSWPressed[2] = true;
 }
 
@@ -29,26 +33,73 @@ void setup() {
   pinMode(SW3_PIN, INPUT_PULLUP);
 
   pinMode(LED_PIN, OUTPUT);
-  //FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, LED_COUNT);
+  FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, LED_COUNT);
 
   attachInterrupt(digitalPinToInterrupt(SW1_PIN), interruptSW1, RISING);
   attachInterrupt(digitalPinToInterrupt(SW2_PIN), interruptSW2, RISING);
   attachInterrupt(digitalPinToInterrupt(SW3_PIN), interruptSW3, RISING);
+  
+  leds[0] = CRGB(0,200,0);
+  leds[1] = CRGB(0,200,0);
+  leds[2] = CRGB(0,200,0);
 }
 
 void loop() {
-  delay(100);
-  if(isSWPressed[0]){
-    Serial.println("button 1 pressed");
-    isSWPressed[0] = false;
-  }
-  if(isSWPressed[1]){
-    Serial.println("button 2 pressed");
-    isSWPressed[1] = false;
-  }
-  if(isSWPressed[2]){
-    Serial.println("button 3 pressed");
-    isSWPressed[2] = false;
+  int i=0;
+  while(Serial.available()){
+    char pcStatus = Serial.read();
+    if(pcStatus >= 48){
+      state[i] = int(pcStatus-48);
+      if(state[i] == 0){
+        leds[i] = CRGB(200,0,0);
+      }else{
+        leds[i] = CRGB(0,200,0);
+      }
+    }
+    i++;
   }
 
+  FastLED.show();
+  if(isSWPressed[0]){
+    Serial.println("0");
+    isSWPressed[0] = false;
+    if(state[0] == 0){
+      state[0] = 1;
+      leds[0] = CRGB(200,0,0);
+    }else{
+      state[0] = 0;
+      leds[0] = CRGB(0,200,0);
+    }
+    FastLED.show();
+    delay(250);
+    attachInterrupt(digitalPinToInterrupt(SW1_PIN), interruptSW1, RISING);
+  }
+  if(isSWPressed[1]){
+    Serial.println("1");
+    isSWPressed[1] = false;
+    if(state[1] == 0){
+      state[1] = 1;
+      leds[1] = CRGB(200,0,0);
+    }else{
+      state[1] = 0;
+      leds[1] = CRGB(0,200,0);
+    }
+    FastLED.show();
+    delay(250);
+    attachInterrupt(digitalPinToInterrupt(SW2_PIN), interruptSW2, RISING);
+  }
+  if(isSWPressed[2]){
+    Serial.println("2");
+    isSWPressed[2] = false;
+    if(state[2] == 0){
+      state[2] = 1;
+      leds[2] = CRGB(200,0,0);
+    }else{
+      state[2] = 0;
+      leds[2] = CRGB(0,200,0);
+    }
+    FastLED.show();
+    delay(250);
+    attachInterrupt(digitalPinToInterrupt(SW3_PIN), interruptSW3, RISING);
+  }
 }
